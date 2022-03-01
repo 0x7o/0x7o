466713cb56e315aa62ef1107b9886046d4d1c179
Danil <me@0x7o.link>
2022-03-01 16:53:05 +0800

    Update core.js

--- gamio/new/core.js
+++ gamio/new/core.js
@@ -1,3 +1,3 @@
-if( window.innerWidth <= 560 ){
-      document.getElementByClassName('image-13').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
+if(window.innerWidth <= 560){
+      document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
  }
