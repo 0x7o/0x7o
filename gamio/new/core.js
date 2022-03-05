24ddebe6e39530e3f4029330642d971fb1f0f82e
Danil <me@0x7o.link>
2022-03-05 17:25:01 +0800

    Update core.js

--- gamio/new/core.js
+++ gamio/new/core.js
@@ -1,4 +1,6 @@
-if(window.innerWidth <= 560){
-      document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
-      console.log("Logo change")
- }
+$(window).on('resize', function(){
+      if(window.innerWidth <= 560){
+            document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
+            console.log("Logo change")
+      }
+)};
