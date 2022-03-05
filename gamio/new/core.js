6eb5409dfe53901f961f8fea0ecd886a903ca160
Danil <me@0x7o.link>
2022-03-05 17:30:31 +0800

    Update core.js

--- gamio/new/core.js
+++ gamio/new/core.js
@@ -2,5 +2,14 @@ $(window).on('resize', function(){
       if(window.innerWidth <= 560){
             document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
             console.log("Logo change")
+      }else if(window.innerWidth >= 991 && window.innerWidth <= 1255){
+            document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
+            console.log("Logo change")
+      }else if(window.innerWidth >= 1256){
+            document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
+            console.log("Logo change")
+      }else{
+            document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
+            console.log("Logo change")
       }
 });
