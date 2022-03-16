0db919bd0ac7fead727a95f02ccbbc47c29f03cb
Danil <me@0x7o.link>
2022-03-16 18:33:03 +0800

    Update core.js

--- gamio/new/core.js
+++ gamio/new/core.js
@@ -1,4 +1,19 @@
+var is_logo = document.querySelectorAll("#logo").length
+
 $(window).on('resize', function(){
+      if(is_logo > 0){
+            if(window.innerWidth <= 560){
+                  document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
+            }else if(window.innerWidth >= 991 && window.innerWidth <= 1255){
+                  document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
+            }else if(window.innerWidth >= 1256){
+                  document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
+            }else{
+                  document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
+            }
+      }
+});
+if(is_logo > 0){
       if(window.innerWidth <= 560){
             document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
             console.log("Logo change")
@@ -7,22 +22,9 @@ $(window).on('resize', function(){
             console.log("Logo change")
       }else if(window.innerWidth >= 1256){
             document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
-            console.log("Logo change")
+            console.log("Logo not change")
       }else{
             document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
-            console.log("Logo change")
+            console.log("Logo not change")
       }
-});
-if(window.innerWidth <= 560){
-      document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
-      console.log("Logo change")
-}else if(window.innerWidth >= 991 && window.innerWidth <= 1255){
-      document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo_sm.svg';
-      console.log("Logo change")
-}else if(window.innerWidth >= 1256){
-      document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
-      console.log("Logo change")
-}else{
-      document.getElementById('logo').src = 'https://0x7o.link/gamio/new/logo.svg';
-      console.log("Logo change")
 }
